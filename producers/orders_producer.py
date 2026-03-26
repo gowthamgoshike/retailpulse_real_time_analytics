2ad2ce4b4c909ac1fcee2bbc52b54527ad159948
gowthamgoshike <gowthamgoshike23@gmail.com>
2026-03-26 09:11:47 -0400

    Kafka producer for retail order events

---
+++ producers/orders_producer.py
@@ -0,0 +1,49 @@
+import json
+import time
+import uuid
+import random
+from kafka import KafkaProducer
+from faker import Faker
+from datetime import datetime
+
+fake = Faker()
+
+producer = KafkaProducer(
+    bootstrap_servers="localhost:9092",
+    value_serializer=lambda v: json.dumps(v).encode("utf-8")
+)
+
+products = [
+    {"id": "P100", "name": "Wireless Mouse", "category": "Electronics", "price": 25.99},
+    {"id": "P101", "name": "Bluetooth Headphones", "category": "Electronics", "price": 89.99},
+    {"id": "P102", "name": "Running Shoes", "category": "Footwear", "price": 120.00},
+    {"id": "P103", "name": "Smart Watch", "category": "Electronics", "price": 199.99},
+    {"id": "P104", "name": "Laptop Backpack", "category": "Accessories", "price": 49.99}
+]
+
+topic = "orders"
+
+while True:
+
+    product = random.choice(products)
+    quantity = random.randint(1,3)
+
+    order_event = {
+        "event_type": "order_created",
+        "order_id": str(uuid.uuid4()),
+        "user_id": fake.uuid4(),
+        "product_id": product["id"],
+        "product_name": product["name"],
+        "category": product["category"],
+        "quantity": quantity,
+        "price": product["price"],
+        "total_amount": quantity * product["price"],
+        "order_status": "confirmed",
+        "order_timestamp": datetime.utcnow().isoformat()
+    }
+
+    producer.send(topic, order_event)
+
+    print("Order Event Sent:", order_event)
+
+    time.sleep(5)
